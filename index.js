b7ab120b7a789a0b8839b4bcabc7a02dad48b27b
parvana22 <vpervane2002@gmail.com>
2024-09-25 20:51:48 +0400

    first login commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// class Car{
+//     constructor(name , speed ){
+//         this.name = name
+//         this.speed = speed   
+//         this.speedUs = speed     
+//     }
+//     accelerate(){
+//         this.speed+=10;
+//         console.log(this.speed)
+//     }
+//     break(){
+//         this.speed-=10;
+//         console.log(this.speed)
+//     }
+//     usZone(){
+//         this.speedUs = this.speed / 1.6
+//         console.log(this.speedUs)
+//     }
+// }
+// const kia = new Car('Kia' ,120);
+// kia.usZone()
+
+
+
+const input = document.querySelector('.input')
+const password = document.querySelector('.password')
+const login = document.querySelector('.login')
+const body = document.querySelector('.body')
+form.addEventListener('submit' , function(){
+    console.log('Submit olundu')
+})
+
+
+
+
+
+
+
+
+
+
+
+
